c02a775666c5de25febac5fb92bf4ca22cae52c9
sem_89 <sem_89@mail.ru>
2022-08-30 00:28:38 +0300

    HomeWork1

---
+++ src/Dog.java
@@ -0,0 +1,5 @@
+public class Dog extends Animal {
+    public Dog(String name) {
+        super("Dog", name, 300, 40, 4);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
         Course c = new Course(new Cross(80), new Water(3), new Wall(5)); // Создаем полосу препятствий
         Team team = new Team("Heroes", new Human("Johny"), new Cat("Murzik"), new Dog("Izzy")); // Создаем команду
